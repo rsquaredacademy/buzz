404bfc86c111abf751b41bb5b81661a53a420752
rsquaredin <rsquaredin@gmail.com>
2022-08-11 23:38:29 +0530

    chore: package details

---
+++ R/buzz.R
@@ -0,0 +1,10 @@
+#' \code{buzz} package
+#' 
+#' Simple notifications for Shiny.
+#' 
+#' See the README on 
+#' \href{https://github.com/rsquaredacademy/buzz}{GitHub}
+#' 
+#' @docType package
+#' @name buzz
+NULL
