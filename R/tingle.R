01d62ca04ce4aff978b8997a5baeb090e4205022
rsquaredin <rsquaredin@gmail.com>
2022-08-12 22:53:50 +0530

    feat: close button

--- R/tingle.R
+++ R/tingle.R
@@ -1,9 +1,18 @@
 #' @export 
-tingle <- function(content = "Hello", session = getDefaultReactiveDomain()) {
+tingle <- function(content = "Hello", close_button = FALSE, button_label = "Close", 
+                   button_type = "default", button_position = "right", 
+                   session = getDefaultReactiveDomain()) {
 
   notice = list(
     content = content
   )
+
+  if (close_button) {
+    notice$footer = TRUE
+    notice$btnLabel = button_label
+    notice$btnType = button_type  
+    notice$btnPosition = button_position
+  }
 
   session$sendCustomMessage(
     type = "tingle.send",
