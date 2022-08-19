39c5102c8daad18ba57262b08a9fb1448b8c347a
rsquaredin <rsquaredin@gmail.com>
2022-08-19 12:06:00 +0530

    docs: notice

--- R/ns.R
+++ R/ns.R
@@ -1,4 +1,42 @@
+#' Notification Styles
+#' 
+#' Simple website notifications with effects
+#' 
+#' @param message Notification message.
+#' @param type Notification type. Default is `"notice"`. Other valid values are: 
+#' \itemize{
+#' \item `"success"`
+#' \item `"warning"`
+#' \item `"error"`
+#' }
+#' @param layout Notification layout. Default is `"growl"`. Other valid values are: 
+#' \itemize{
+#' \item `"attached"`
+#' \item `"bar"`
+#' }
+#' @param effect Notification effect type. Valid values include:  
+#' \itemize{
+#' \item For `"growl" layout`
+#' \itemize{
+#' \item `"scale"`
+#' \item `"jelly"`
+#' \item `"slide"`
+#' \item `"genie"` 
+#' } 
+#' \item For `"attached" layout`
+#' \itemize{
+#' \item `"flip"`  
+#' \item `"bouncyflip"`
+#' }
+#' \item For `"bar" layout`
+#' \itemize{
+#' \item `"slidetop"` 
+#' \item `"exploader"` 
+#' }
+#' }
+#' 
 #' @export
+#' 
 notice <- function(message = "Hello", type = "notice", layout = "growl",
                    effect = "jelly", session = getDefaultReactiveDomain()) {
 
